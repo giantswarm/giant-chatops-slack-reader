967d1a05d6d6b7c65994ee2dd1b09e5a6db7b8fa
Marian Steinbach <marian@giantswarm.io>
2021-06-14 16:25:39 +0200

    First code (#1)
    * Adapt README
    
    * First code
    
    * Update CHANGELOG.md
    
    * Add .circleci/config.yml
    
    * Tidy up
    
    * Update architect-orb
    
    * Add docker build
    
    * Remove obsolete step in Dockerfile

--- README.md
+++ README.md
@@ -1,30 +1,15 @@
-[![CircleCI](https://circleci.com/gh/giantswarm/template.svg?style=shield)](https://circleci.com/gh/giantswarm/template)
+[![CircleCI](https://circleci.com/gh/giantswarm/giant-chatops-slack-reader.svg?style=shield)](https://circleci.com/gh/giantswarm/giant-chatops-slack-reader)
 
-# REPOSITORY_NAME
+# giant-chatops Slack Reader
 
-This is a template repository containing some basic files every repository
-needs.
+This little utility is triggered with a Slack channel ID and a Slack token,
+to
 
-To use it just hit `Use this template` button or [this link][generate].
+- go and look for alert details
+- pass these details on for use in Argo Workflows
+- post some status message back into the channel
 
-Things to do with your newly created repo:
+Required environment variables:
 
-1. Run`devctl replace -i "REPOSITORY_NAME" "$(basename $(git rev-parse
-   --show-toplevel))" --ignore '.git/**' '**'`.
-2. Run `devctl replace -i "template" "$(basename $(git rev-parse
-   --show-toplevel))" --ignore '.git/**' '**'`.
-3. Go to https://github.com/giantswarm/REPOSITORY_NAME/settings and make sure `Allow
-   merge commits` box is unchecked and `Automatically delete head branches` box
-   is checked.
-4. Go to https://github.com/giantswarm/REPOSITORY_NAME/settings/access and add
-   `giantswarm/bots` with `Write` access and `giantswarm/employees` with
-   `Admin` access.
-5. Add this repository to https://github.com/giantswarm/github.
-6. Create quay.io docker repository if needed.
-7. Add the project to the CircleCI:
-   https://circleci.com/setup-project/gh/giantswarm/REPOSITORY_NAME
-8. Change the badge (with style=shield):
-   https://circleci.com/gh/giantswarm/REPOSITORY_NAME.svg?style=shield&circle-token=TOKEN_FOR_PRIVATE_REPO
-   If this is a private repository token with scope `status` will be needed.
-
-[generate]: https://github.com/giantswarm/template/generate
+- `SLACK_TOKEN`: The Slack auth token
+- `SLACK_CHANNEL_ID`: ID of the Slack channel this program should use
--- main.go
+++ main.go
@@ -1,3 +1,119 @@
+// Package main is a little Slack client kumping into a freshly created channel, looking
+// for messages containing information about an incident.
 package main
 
-func main() {}
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/slack-go/slack"
+
+	"github.com/giantswarm/giant-chatops-slack-reader/pkg/messageparser"
+)
+
+const (
+	argoWebhookURL = "http://giant-chatops-alert-eventsource-svc:12000/alert"
+)
+
+func main() {
+	slackToken := os.Getenv("SLACK_TOKEN")
+	if slackToken == "" {
+		fmt.Println("ERROR: Environment variable SLACK_TOKEN must be set.")
+		os.Exit(0)
+	}
+
+	slackChannelID := os.Getenv("SLACK_CHANNEL_ID")
+	if slackChannelID == "" {
+		fmt.Println("ERROR: Environment variable SLACK_CHANNEL_ID must be set.")
+		os.Exit(0)
+	}
+
+	fmt.Printf("Channel ID: %s\n", slackChannelID)
+
+	api := slack.New(slackToken, slack.OptionDebug(true))
+
+	// Find out channel name
+	channel, err := api.GetConversationInfo(slackChannelID, false)
+	if err != nil {
+		fmt.Printf("ERROR: Cannot get channel details - %#v\n", err)
+		os.Exit(0)
+	}
+	fmt.Printf("Channel name: %#v\n", channel.Name)
+	if !strings.HasPrefix(channel.Name, "inc-") {
+		fmt.Println("Ignoring this channel, as the name does not start with 'inc-'. Exiting.\n", channel.Name)
+		os.Exit(0)
+	}
+
+	// Simply wait for 5 seconds, so there is enough time for the alert details to appear.
+	time.Sleep(5 * time.Second)
+
+	_, _, _, err = api.JoinConversation(slackChannelID)
+	if err != nil {
+		fmt.Printf("ERROR: Could not join channel %s: %#v\n", slackChannelID, err)
+		os.Exit(0)
+	}
+
+	// We set this to prevent processing the same channel multiple times.
+	doneIdentifier := fmt.Sprintf("[giant-chatops-slack-reader done for channel %s]", slackChannelID)
+
+	history, err := api.GetConversationHistory(&slack.GetConversationHistoryParameters{ChannelID: slackChannelID})
+	if err != nil {
+		fmt.Printf("ERROR: Could not get conversation history: %#v\n", err)
+		os.Exit(0)
+	}
+
+	parseResult, err := messageparser.ParseConversationHistory(history, doneIdentifier)
+	if err != nil {
+		fmt.Printf("ERROR: Could not parse conversation history: %#v\n", err)
+		os.Exit(0)
+	}
+
+	parseResult.SlackChannelID = slackChannelID
+	parseResult.SlackChannelName = channel.Name
+
+	// Trigger event via webhook. Collect any errors in eventError.
+	var eventError error
+	if parseResult.IsAlert {
+		jsonBytes, err := json.Marshal(parseResult)
+		if err == nil {
+			resp, err := http.Post(argoWebhookURL, "application/json", bytes.NewBuffer(jsonBytes))
+			if err != nil {
+				eventError = err
+			} else if resp.StatusCode != http.StatusOK {
+				eventError = fmt.Errorf("could not trigger alert event, webhook status was %d", resp.StatusCode)
+			}
+		} else {
+			eventError = err
+		}
+	}
+
+	// Give feedback to channel
+	text := ""
+	if eventError != nil {
+		text = fmt.Sprintf("Could not trigger alert event: `%s`", eventError)
+	} else {
+		if parseResult.IsAlert {
+			text = "I'm passing these alert details to my co-bots to gather some information for you:\n\n"
+			jsonBytes, err := json.MarshalIndent(parseResult, "", "    ")
+			if err != nil {
+				text += fmt.Sprintf("Problem writing JSON: `%s`", err)
+			} else {
+				text += fmt.Sprintf("```%s```\n", string(jsonBytes))
+			}
+
+			text += fmt.Sprintf("\n\n`%s`", doneIdentifier)
+		} else {
+			text = "No alert info found so far. Please share an #opsgenie alert message in this channel."
+		}
+	}
+
+	_, _, _, err = api.SendMessage(slackChannelID, slack.MsgOptionText(text, false))
+	if err != nil {
+		fmt.Printf("ERROR: Could not post feedback: %#v\n", err)
+	}
+}
